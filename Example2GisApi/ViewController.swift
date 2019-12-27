c1e33fa07e2d751ef4a70c70964628922b9b6d24
Alex Smetannikov <alexsmetdev@gmail.com>
2019-12-27 14:33:05 +0300

    Initial Commit

---
+++ Example2GisApi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example2GisApi
+//
+//  Created by Aleksandr Smetannikov on 27/12/2019.
+//  Copyright © 2019 Aleksandr Smetannikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
